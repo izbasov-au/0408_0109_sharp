3264f5ce49da94a620f4710c668755af342554aa
jakkerman <jakkerman@mail.ru>
2022-08-04 23:21:22 +0500

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,10 @@
+﻿//task1
+Console.Write("Введите два целых числа: ");
+int a = int.Parse(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (b * b == a) {
+    Console.WriteLine("Да");
+}
+else {
+    Console.WriteLine("Нет");
+}
